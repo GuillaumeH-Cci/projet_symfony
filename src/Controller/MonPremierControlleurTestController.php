ce089362053a051550d45743d1faa53168d69e13
GuillaumeH-Cci <guillaume.hess@ccicampus.fr>
2026-04-10 13:31:23 +0200

    10/04/26 - Modif Route

--- src/Controller/MonPremierControlleurTestController.php
+++ src/Controller/MonPremierControlleurTestController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 final class MonPremierControlleurTestController extends AbstractController
 {
-    #[Route('/mon/premier/controlleur/test', name: 'app_mon_premier_controlleur_test')]
+    #[Route('/partieTest', name: 'app_mon_premier_controlleur_test')]
     public function index(): Response
     {
         return $this->render('mon_premier_controlleur_test/index.html.twig', [
