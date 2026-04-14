c434573f7b123401ab95dfd3b007c01ad3ccfd1e
GuillaumeH-Cci <guillaume.hess@ccicampus.fr>
2026-04-14 16:02:54 +0200

    14/04/26 - Ajout formulaire

---
+++ src/Controller/ContactController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\ContactFormType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+final class ContactController extends AbstractController
+{
+    #[Route('/contact', name: 'app_contact')]
+    public function index(Request $request): Response
+    {
+        $form = $this->createForm(ContactFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $data = $form->getData();
+
+            $this->addFlash('success', 'Message envoyé avec succès !');
+
+            return $this->redirectToRoute('app_contact');
+        }
+
+        return $this->render('contact/index.html.twig', [
+            'contactForm' => $form->createView(),
+        ]);
+    }
+}
