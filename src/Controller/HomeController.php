a70da6bd6068688b08b4b9851fa55068b7d43a83
GuillaumeH-Cci <guillaume.hess@ccicampus.fr>
2026-04-16 09:16:40 +0200

    16/04/26 - Modif redirection

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -6,7 +6,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/home', name: 'app_home')]
+
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
