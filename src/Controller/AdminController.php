877180588fdfb392fd039b7efa84500da7e24fe7
GuillaumeH-Cci <guillaume.hess@ccicampus.fr>
2026-04-17 11:22:34 +0200

    17/04/26 - Pagination pour Utilisateur et Articles

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -2,8 +2,10 @@
 
 namespace App\Controller;
 
+use Knp\Component\Pager\PaginatorInterface;
 use App\Repository\UserRepository;
 use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -11,15 +13,32 @@ use Symfony\Component\Routing\Attribute\Route;
 final class AdminController extends AbstractController
 {
     #[Route('/admin', name: 'app_admin')]
-    public function index(ArticleRepository $articleRepository, UserRepository $userRepository): Response
-    {
-        $users = $userRepository->findAll();
-        $articles = $articleRepository->findAll();
+    public function index(
+    UserRepository $userRepo, 
+    ArticleRepository $articleRepo, 
+    PaginatorInterface $paginator, 
+    Request $request 
+    ): Response {
+    
+    $searchName = $request->query->get('search_name', '');
 
-        return $this->render('admin/index.html.twig', [
-            'controller_name' => 'AdminController',
-            'users' => $users,
-            'articles' => $articles,  
-        ]);
-    }
+    $users = $paginator->paginate(
+        $userRepo->findAll(), 
+        $request->query->getInt('pageUsers', 1),
+        5
+    );
+
+    $articles = $paginator->paginate(
+        $articleRepo->findAll(),
+        $request->query->getInt('pageArticles', 1),
+        5
+    );
+
+    // 4. ENVOI À LA VUE
+    return $this->render('admin/index.html.twig', [
+        'users' => $users,
+        'articles' => $articles,
+        'searchName' => $searchName, 
+    ]);
+}   
 }
