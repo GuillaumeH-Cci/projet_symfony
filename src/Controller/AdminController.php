1198957a559754c551efc5a7460339080fd912de
GuillaumeH-Cci <guillaume.hess@ccicampus.fr>
2026-04-17 11:49:40 +0200

    17/04/26 - Modif minim

--- src/Controller/AdminController.php
+++ src/Controller/AdminController.php
@@ -10,7 +10,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[IsGranted('ROLE_ADMIN')]
 final class AdminController extends AbstractController
 {
     
