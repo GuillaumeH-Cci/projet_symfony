26998d5373723a3e703d52199f657520a079cd39
GuillaumeH-Cci <guillaume.hess@ccicampus.fr>
2026-04-21 12:32:47 +0200

    Modif pour créateur aléatoire

--- src/Story/AppStory.php
+++ src/Story/AppStory.php
@@ -2,6 +2,7 @@
 
 namespace App\Story;
 
+use App\Factory\ArticleFactory;
 use Zenstruck\Foundry\Attribute\AsFixture;
 use Zenstruck\Foundry\Story;
 
@@ -13,9 +14,15 @@ final class AppStory extends Story
 {
     public function build(): void
     {
-        UserStory::load();
-        CatStory::load();
-        PlatStory::load();
-        GameStory::load();
-    }
+    // Créer 10 articles avec des utilisateurs, catégories et plateformes aléatoires
+    $games = ArticleFactory::createMany(10, function () {
+        return [
+            'usr' => UserStory::getRandom('users'),
+            'cat' => CatStory::getRandom('categories'),
+            'plat' => PlatStory::getRandomRange('plateformes', 1, 3),
+        ];
+    });
+
+    $this->addState('games', $games, 'games');
+    }    
 }
